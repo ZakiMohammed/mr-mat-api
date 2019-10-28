660a8a5391901a7d3ba0512ea457ebef828e95a8
Zaki Mohammed Shaikh <ZakiS@citiustech.com>
2019-10-28 19:01:58 +0530

    Customer model updated

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -4,8 +4,16 @@ namespace mr_mat_api.Models
     {
         public long Id { get; set; }
         public string Name { get; set; }
-        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+        public int PinCode { get; set; }
         public string Mobile { get; set; }
         public string Email { get; set; }
+        public string Gender { get; set; }
+        public System.DateTime DateOfBirth { get; set; }
+        public string Notification { get; set; }
+        public int WorkLocationDistance { get; set; }
+        public bool IsPrivate { get; set; }
     }
 }
